cece768312094ca9e37ae78d9b1d9405ecbc9cba
Coolb0x <45721224+Coolb0x@users.noreply.github.com>
2025-03-28 19:14:06 +0200

    Refactor app.ts: update comments for clarity and remove unused code from app_old1.ts

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackBotToken = process.env.SLACK_BOT_TOKEN;
 const openaiApiKey = process.env.OPENAI_API_KEY;
 
-/** Slack App Initialization */
+// Slack App Initialization
 const app = new App({
   token: slackBotToken,
   signingSecret: slackSigningSecret,
@@ -14,15 +14,12 @@ const app = new App({
   // logLevel: LogLevel.DEBUG, // or LogLevel.INFO
 });
 
-/** OpenAI Setup */
+// OpenAI Setup
 const openai = new OpenAI({
   apiKey: openaiApiKey,
 });
 
-//No system message for better results
-// const DEFAULT_SYSTEM_CONTENT = ``;
-
-// !Store thread IDs per channel/thread combination
+// Store thread IDs per channel/thread combination
 const threadContexts: { [key: string]: any } = {};
 
 async function runAssistant(assistantId: string, threadId: any, userInput: string) {
@@ -33,12 +30,12 @@ async function runAssistant(assistantId: string, threadId: any, userInput: strin
       content: userInput,
     });
 
-    // 3. Run the Assistant
+    // Run the Assistant
     const run = await openai.beta.threads.runs.create(threadId, {
       assistant_id: assistantId,
     });
 
-    // 4. Check the Run status and retrieve the Assistant's response
+    // Check the Run status and retrieve the Assistant's response
     let runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
 
     while (
@@ -52,7 +49,7 @@ async function runAssistant(assistantId: string, threadId: any, userInput: strin
     }
 
     if (runStatus.status === "completed") {
-      // 5. Retrieve Messages
+      // Retrieve Messages
       const messages = await openai.beta.threads.messages.list(threadId);
 
       const assistantMessages: AssistantMessageData[] = messages.data.filter(
@@ -75,15 +72,7 @@ async function runAssistant(assistantId: string, threadId: any, userInput: strin
 }
 
 const assistant = new Assistant({
-  userMessage: async ({
-    client,
-    logger,
-    message,
-    getThreadContext,
-    say,
-    setTitle,
-    setStatus,
-  }: UserMessageParams) => {
+  userMessage: async ({ client, logger, message, say, setTitle, setStatus }: UserMessageParams) => {
     const { channel, thread_ts } = message;
     const contextKey = `${channel}-${thread_ts}`;
 
@@ -106,27 +95,6 @@ const assistant = new Assistant({
         oldest: thread_ts,
       });
 
-      //! This adds context directly to the thread via the slack conversation and will pass to openai the whole thread as one message
-      // ! it is not needed as the logic here is to create one threadId and each consecutive message will be added to the thread
-      //   let messages;
-
-      //     if (thread.messages.length <= 1) {
-      //     messages = [
-      //       { role: "system", content: DEFAULT_SYSTEM_CONTENT },
-      //       { role: "user", content: message.text },
-      //     ];
-      //   } else {
-      //       const threadHistory = thread.messages.slice(0, -1).map(m => {
-      //       const role = m.bot_id ? "assistant" : "user";
-      //       return { role, content: m.text };
-      //     });
-      //     messages = [
-      //       { role: "system", content: DEFAULT_SYSTEM_CONTENT },
-      //       ...threadHistory,
-      //       { role: "user", content: message.text },
-      //     ];
-      //   }
-
       // Use the existing threadId
       const response = await runAssistant("asst_Bcy8adckdCKjQOPT2Gebjz5K", threadId, message.text);
 
@@ -136,7 +104,7 @@ const assistant = new Assistant({
       await say({ text: "Sorry, something went wrong!" });
     }
   },
-  // The below threadStarted is the default behavior when a thread is started and required by the Assistant, so only logger is added
+  // The below threadStarted is the default behavior when a thread is started and required by the Assistant
 
   threadStarted: async ({ logger, message, client }: { logger: any; message: any; client: any }) => {
     // JUST to debug / remove later
